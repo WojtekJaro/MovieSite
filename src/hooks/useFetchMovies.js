bee0cd78b8c2869dba6a9a1cc946f3675d7d0e37
Wojtek <wwojciechjarocki@gmail.com>
2024-12-04 19:56:19 +0100

    data fetch bugfix

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Box } from '@mui/material'
 const useFetchMovies = query => {
@@ -14,15 +14,14 @@ const useFetchMovies = query => {
 			const response = await axios.get(
 				`https://api.themoviedb.org/3/movie/${query}?api_key=${process.env.REACT_APP_API_KEY}`
 			)
-			console.log(response)
 			setData(response.data.results)
 		} catch (error) {
-			console.log(error)
-			setError('nie udało się pobrać danych')
+			setError('wystąpil bląd')
 		}
 	}
+  
 
-	return <Box></Box>
+	return { data, loading, error }
 }
 
 export default useFetchMovies
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import useFetchMovies from '../hooks/useFetchMovies'
 import MainTemplate from '../template/MainTemplate'
 import Header from '../components/Header'
-import { Box, Container } from '@mui/material'
-import useFetchMovies from '../hooks/useFetchMovies'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import { Pagination } from 'swiper/modules'
+
+import { Box, Container, Typography } from '@mui/material'
+import { AspectRatio, Image } from '@mui/icons-material'
 
 const HomePage = () => {
-  const topRated = useFetchMovies("top_rated");
-  return (
-    <MainTemplate>
-        <Header/>
-        <Box>
-          <h2>siema</h2>
-        </Box>
-    </MainTemplate>
-  )
+	const topRated = useFetchMovies('top_rated')
+	console.log(topRated)
+	if (topRated.data.length === 0) {
+		return
+	}
+
+	return (
+		<MainTemplate>
+			<Header />
+			<Box>
+				<Typography level='h1'>{topRated.data[1].title}</Typography>
+			</Box>
+		</MainTemplate>
+	)
 }
 
 export default HomePage
